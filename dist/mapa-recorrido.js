9aef5cd14b042cb6e972a4ebccf37f9b737a703c
RickySSD <ricky6982@gmail.com>
2016-02-09 01:07:07 -0300

    Se agrego la funcion que actualiza las conexiones entre nodos cuando crea una trayectoria.
    Signed-off-by: RickySSD <ricky6982@gmail.com>

--- dist/mapa-recorrido.js
+++ dist/mapa-recorrido.js
@@ -571,7 +571,7 @@ _path = {
                 // Verificamos si existe un arco entre un nodo y el nodo siguiente
                 if ($.inArray(trayecto[i+1], _network.getConnectedNodes(trayecto[i])) == -1) {
                     _edge.add(_edge.create(trayecto[i], trayecto[i+1]));
-                    // actualizarConexionOrientacion(trayecto[i], trayecto[i+1]);
+                    actualizarConexionOrientacion(trayecto[i], trayecto[i+1]);
                 }
             }
         }
